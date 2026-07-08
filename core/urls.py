0ca4ab072445f04bb7c013dd68c60bbe6c6819ae
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2026-07-08 12:59:38 +0530

    authentication

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-from .views import home, create_job, delete_job, update_job
+from . import views
 
 urlpatterns = [
-    path("", home, name="home"),
-    path("jobs/", create_job, name="create-job"),
-    path("jobs/<int:id>/delete", delete_job, name="delete-job"),
-    path("jobs/<int:id>/updates", update_job, name="update-job"),
+    path("", views.home, name="home"),
+    path("jobs/", views.create_job, name="create-job"),
+    path("jobs/<int:id>/delete", views.delete_job, name="delete-job"),
+    path("jobs/<int:id>/updates", views.update_job, name="update-job"),
+    path("register/", views.register_user, name="register-user"),
+    path("login/", views.login_user, name="login-user"),
+    path("logout/", views.logout_user, name="logout-user"),
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render, redirect
 from .models import Job
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
 
+@login_required
 def home(request):
     jobs = Job.objects.all()
     return render(request, "index.html", {"jobs": jobs})
 
 
+@login_required
 def create_job(request):
     if request.method == "POST":
         Job.objects.create(
@@ -24,6 +30,7 @@ def create_job(request):
         return render(request, "jobform.html")
 
 
+@login_required
 def delete_job(request, id):
     if request.method == "POST":
         job = Job.objects.get(id=id)
@@ -31,6 +38,7 @@ def delete_job(request, id):
     return redirect("/")
 
 
+@login_required
 def update_job(request, id):
     job = Job.objects.get(id=id)
     if request.method == "POST":
@@ -46,3 +54,48 @@ def update_job(request, id):
         return redirect("/")
 
     return render(request, "jobform.html", {"job": job})
+
+
+def register_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        exist = User.objects.filter(username=username).exists()
+        if exist:
+            messages.warning(
+                request=request, message="Already registered user, please try login"
+            )
+            return redirect("/login")
+        password = request.POST.get("password")
+        User.objects.create_user(username=username, password=password)
+        messages.success(request=request, message="registration successfull")
+        return redirect("/login")
+    return render(request, "registration.html")
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = User.objects.filter(username=username).first()
+        if not user:
+            messages.error(request=request, message="user not registered")
+            return redirect("/register")
+
+        authenticated_user = authenticate(request, username=username, password=password)
+        if not authenticated_user:
+            messages.error(request=request, message="invalid username or password")
+            return redirect("/login")
+
+        login(request=request, user=authenticated_user)
+        messages.success(
+            request=request, message=f"Welcome back {request.user.username}"
+        )
+        return redirect("/")
+    return render(request, "login.html")
+
+
+def logout_user(request):
+    logout(request=request)
+    messages.success(request=request, message="Logout Succesfull")
+    return redirect("/login")
